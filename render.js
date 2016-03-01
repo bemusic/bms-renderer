2fc8a0dfb3ba8d6714e502a1c00a0441b5ce5cc6
Thai Pangsakulyanont <dtinth@spacet.me>
2016-03-02 00:04:02 +0700

    add circleci build

--- render.js
+++ render.js
@@ -108,7 +108,7 @@ const renderers = {
 }
 
 function canUseBmsampler () {
-  const BMSAMPLER_VERSION = '^0.1.0'
+  const BMSAMPLER_VERSION = '^0.2.0'
   try {
     const output = childProcess.execFileSync('bmsampler', [ '-v' ], { input: '', encoding: 'utf8' }).trim()
     if (satisfies(output, BMSAMPLER_VERSION)) {
